adbf037d1f52564421d9b98e5a4493ce1109fb37
ImRayy <souradeep20roy01@gmail.com>
2023-05-05 20:05:43 +0530

    Updated Header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,18 +4,21 @@ import Button from "./Button";
 import React from "react";
 
 interface HeaderProps {
-  title?: string;
-  amount?: string;
-  currency?: "inr" | "dollar";
+  isFixed: boolean;
+  buttonFunc?: () => void;
 }
-const Header = ({ title, amount, currency }: HeaderProps) => {
+const Header = ({ isFixed, buttonFunc }: HeaderProps) => {
   const router = useRouter();
   return (
-    <div className="fixed left-0 top-0 z-50 flex w-full items-center text-xl font-bold backdrop-blur-sm">
+    <div
+      className={`${
+        isFixed ? "fixed left-0 top-0 z-50 backdrop-blur-sm" : "block"
+      } flex w-full items-center text-xl font-bold `}
+    >
       <div className="flex w-full items-center justify-between p-4">
         <span className="flex items-center gap-4">
           {router.pathname !== "/" ? (
-            <Button variant="secondary" size="equal">
+            <Button variant="secondary" size="equal" onClick={buttonFunc}>
               <ChevronLeft />
             </Button>
           ) : (
@@ -24,11 +27,13 @@ const Header = ({ title, amount, currency }: HeaderProps) => {
         </span>
         <span className="flex items-center gap-2">
           <span className="rounded-full border-2 px-4 py-1 text-lg">May</span>
-          <img
-            src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
-            alt="profile"
-            className="h-10 w-10 rounded-full"
-          />
+          {router.pathname === "/" && (
+            <img
+              src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+              alt="profile"
+              className="h-10 w-10 rounded-full"
+            />
+          )}
         </span>
       </div>
     </div>
