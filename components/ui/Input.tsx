e847965502788f953faca28b8c072d6774be656d
ImRayy <souradeep20roy01@gmail.com>
2023-05-14 23:30:14 +0530

    Updated Input

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,23 +1,27 @@
 import React, { InputHTMLAttributes } from "react";
 const Input = ({
   id,
+  label,
   textSize,
   ...rest
 }: InputHTMLAttributes<HTMLInputElement> & {
   id: string;
+  label?: string;
   textSize?: string;
 }) => {
   return (
-    <input
-      id={id}
-      type="text"
-      className={
-        `w-full border-b border-gray-300 border-x-transparent border-t-transparent py-2 focus:border-gray-300 focus:border-x-transparent focus:border-t-transparent focus:outline-none focus:ring-0` +
-        " " +
-        textSize
-      }
-      {...rest}
-    />
+    <label htmlFor={id}>
+      <div className="mb-2 pl-2 capitalize">{label}</div>
+      <input
+        id={id}
+        className={
+          `border-b border-gray-300 border-x-transparent border-t-transparent py-2 focus:border-gray-300 focus:border-x-transparent focus:border-t-transparent focus:outline-none focus:ring-0` +
+          " " +
+          textSize
+        }
+        {...rest}
+      />
+    </label>
   );
 };
 
