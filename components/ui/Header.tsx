6464977ed3cce7557f843e90ebb3c6204891db50
ImRayy <souradeep20roy01@gmail.com>
2023-05-15 00:17:13 +0530

    Updated header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -6,9 +6,17 @@ import React from "react";
 interface HeaderProps {
   isFixed: boolean;
   customTxt?: string;
+  showPathName?: boolean;
+  customButtonClass?: string;
   buttonFunc?: () => void;
 }
-const Header = ({ isFixed, customTxt, buttonFunc }: HeaderProps) => {
+const Header = ({
+  isFixed,
+  customTxt,
+  showPathName,
+  customButtonClass,
+  buttonFunc,
+}: HeaderProps) => {
   const router = useRouter();
   return (
     <div
@@ -19,15 +27,29 @@ const Header = ({ isFixed, customTxt, buttonFunc }: HeaderProps) => {
       <div className="flex w-full items-center justify-between p-4">
         <span className="flex items-center gap-4">
           {router.pathname !== "/" ? (
-            <Button variant="secondary" size="equal" onClick={buttonFunc}>
+            <Button
+              variant="secondary"
+              size="equal"
+              className={`${customButtonClass} relative z-20 `}
+              onClick={() => router.back()}
+            >
               <ChevronLeft />
             </Button>
           ) : (
             <span>{customTxt || "Hi, Ray"}</span>
           )}
         </span>
+
+        {/* Pathname */}
+        {showPathName && (
+          <div className="absolute w-full text-center font-medium capitalize tracking-wide text-white">
+            {router.pathname.split("/")[1]}
+          </div>
+        )}
         <span className="flex items-center gap-2">
-          <span className="rounded-full border-2 px-4 py-1 text-lg">May</span>
+          {!showPathName && (
+            <span className="rounded-full border-2 px-4 py-1 text-lg">May</span>
+          )}
           {router.pathname === "/" && (
             <img
               src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
